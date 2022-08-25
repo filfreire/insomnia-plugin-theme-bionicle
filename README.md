68a46d1f8e61dff597de1c4f6ba97a046baf9a80
Filipe Freire <livrofubia@gmail.com>
2022-08-25 10:08:40 +0100

    Add initial changes

---
+++ README.md
@@ -0,0 +1,21 @@
+# Insomnia Bionicle theme
+
+Theme for insomnia that is using default "Core Default" colors and adds Toa Tahu image to the left bottom corner.
+
+Image can be changed by replacing `tahu.png` file (must have same name and format!)
+
+Adapted from https://github.com/Kubas556/insomnia-plugin-luna-theme for example purposes
+
+## Preview
+
+
+
+## Image used in plugin
+
+<img style="width:50%" src="./tahu.png">
+
+Repository adapted from https://github.com/Kubas556/insomnia-plugin-luna-theme
+Image originally from : https://vsbattles.fandom.com/wiki/Tahu
+
+> "United in Duty. Bound in Destiny. This is the way... of the Bionicle!"
+> — Turaga Vakama, BIONICLE 2: Legends of Metru Nui
--- main.js
+++ main.js
@@ -1,4 +1,130 @@
 // For help writing plugins, visit the documentation to get started:
-//   https://docs.insomnia.rest/insomnia/introduction-to-plugins
+//   https://support.insomnia.rest/article/173-plugins
 
-// TODO: Add plugin code here...
+const fs = require("fs");
+const os = require("os");
+
+const platform = os.platform()
+const homeDir = os.homedir()
+let path = ""
+switch (platform) {
+    case "win32":
+        path = homeDir+"/AppData/Roaming/Insomnia/plugins/insomnia-plugin-theme-bionicle/tahu.png"
+        break
+    case "darwin":
+        path = homeDir+"/Library/Application Support/Insomnia/plugins/insomnia-plugin-theme-bionicle/tahu.png"
+        break;
+    default:
+        throw "Unsupported platform!"
+}
+
+let image = fs.readFileSync(path);
+let stringImage = image.toString("base64");
+
+module.exports.themes = [{
+    name: "theme-bionicle",
+    displayName: "Bionicle theme",
+    theme: {
+        rawCss: `
+    .request-pane.theme--pane.pane .pane__body.theme--pane__body.react-tabs {
+        background-image: url(data:image/png;base64,${stringImage});
+        background-position-x: 5rem;
+        background-position-y: bottom;
+        background-size: 70%;
+        background-repeat: no-repeat;
+    }
+    /*#BionicleOverlay {
+        width:100%;
+        height:100%;
+        position:absolute;
+        top:0;
+        pointer-events:none;
+        background-image: url(data:image/png;base64,${stringImage});
+        background-position-x: calc(1vw + 1rem);
+        background-position-y: bottom;
+        background-size: 49vmin;
+        background-repeat: no-repeat;
+        z-index:20;
+    }*/
+    `,
+        styles: {
+            transparentOverlay: {
+                background: {
+                    default: 'rgba(30, 30, 30, 0.8)',
+                },
+                foreground: {
+                    default: '#ddd',
+                },
+            },
+            dialog: {
+                background: {
+                    default: '#fff',
+                },
+                foreground: {
+                    default: '#333',
+                },
+            },
+            appHeader: {
+                background: {
+                    default: '#2C2C2C',
+                },
+            },
+            sidebar: {
+                background: {
+                    default: '#2C2C2C',
+                    success: '#7ecf2b',
+                    notice: '#f0e137',
+                    warning: '#ff9a1f',
+                    danger: '#ff5631',
+                    surprise: '#a896ff',
+                    info: '#46c1e6',
+                },
+                foreground: {
+                    default: '#e0e0e0',
+                },
+                highlight: {
+                    default: '#999',
+                },
+            },
+            sidebarHeader: {
+                background: {
+                    default: '#695eb8',
+                },
+                foreground: {
+                    default: '#fff',
+                },
+            },
+            paneHeader: {
+                foreground: {
+                    default: '#ccc',
+                },
+                background: {
+                    default: '#212121',
+                    success: '#75ba24',
+                    notice: '#d8c84d',
+                    warning: '#ec8702',
+                    danger: '#e15251',
+                    surprise: '#8776d5',
+                    info: '#20aed9',
+                },
+            },
+            pane: {
+                background: {
+                    default: '#292929',
+                    success: '{{ styles.sidebar.background.success }}',
+                    notice: '{{ styles.sidebar.background.notice }}',
+                    warning: '{{ styles.sidebar.background.warning }}',
+                    danger: '{{ styles.sidebar.background.danger }}',
+                    surprise: '{{ styles.sidebar.background.surprise }}',
+                    info: '{{ styles.sidebar.background.info }}',
+                },
+                foreground: {
+                    default: '#e0e0e0',
+                },
+                highlight: {
+                    default: '#999',
+                },
+            },
+        },
+    }
+}]
